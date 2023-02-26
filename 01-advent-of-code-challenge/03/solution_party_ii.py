de0d1cdc3b1684150586d54740f00ce6897c6b3e
MatheusMatsuo <69215288+MatheusMatsuo@users.noreply.github.com>
2023-02-26 18:46:00 -0300

    Update solution_party_ii.py

--- 01-advent-of-code-challenge/03/solution_party_ii.py
+++ 01-advent-of-code-challenge/03/solution_party_ii.py
@@ -1,19 +1,33 @@
-arquivo = open('mario1.txt', 'r');
-
-caloriasPorElfo = []
-aux = 0
-caloriasMaioresTresElfos = 0
+arquivo = open('ex4.txt', 'r');
+par1 = []
+par2= []
+parNum1 = []
+parNum2 = []
+paresAtrib = 0
+paresAtrib2 = 0
 
 for linha in arquivo:
-  if linha != '\n':
-    aux = aux + int(linha)
-  else:
-    caloriasPorElfo.append(aux)
-    aux= 0
+  linha = linha.replace('\n','')
+  aux = linha.split(',')
+  aux1 = aux[0].split('-')
+  aux2 = aux[1].split('-')
+  par1.append(aux1)
+  par2.append(aux2)
 
-for i in range(3):
-      maiorCaloria = max(caloriasPorElfo)
-      caloriasMaioresTresElfos += maiorCaloria
-      caloriasPorElfo.remove(maiorCaloria)
+for i in par1:
+  parNum1.append(int(i[0]))
+  parNum1.append(int(i[1]))
 
-print("A soma de calorias dos 3 maiores elfos é de " + str(caloriasMaioresTresElfos) + " calorias")
+for i in par2:
+  parNum2.append(int(i[0]))
+  parNum2.append(int(i[1]))
+
+for i in range(0, len(parNum1), 2):
+  if parNum1[i] == parNum2[i] or parNum1[i] == parNum2[i + 1] or parNum1[i + 1] == parNum2[i] or parNum1[i + 1] == parNum2[i + 1]:
+    paresAtrib2 += 1
+  elif parNum1[i] > parNum2[i] and parNum1[i] < parNum2[i + 1]:
+    paresAtrib2 += 1
+  elif parNum1[i] < parNum2[i] and parNum1[i + 1] > parNum2[i]:
+    paresAtrib2 += 1 
+
+print(paresAtrib2) 
