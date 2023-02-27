f0f75861c977e86aa9900c1e837a992230e459af
MatheusMatsuo <69215288+MatheusMatsuo@users.noreply.github.com>
2023-02-26 21:02:22 -0300

    Update solution-party-i.py

--- 01-advent-of-code-challenge/06/solution-party-i.py
+++ 01-advent-of-code-challenge/06/solution-party-i.py
@@ -1,38 +1,19 @@
-arquivo = open('jaquempo.txt', 'r');
-
-adversario = []
-eu = []
-pontos = 0
-
+arquivo = open('ex6.txt', 'r');
+arrayPacote = []
 for linha in arquivo:
-  adversario.append(linha[0])
-  eu.append(linha[2])
-
-for jogada in range(len(adversario)):
-    if adversario[jogada] == "A":
-      if eu[jogada] == "Y":
-        pontos += 6 + 2
-      elif eu[jogada] == "Z":
-        pontos += 0 + 3
-      else:
-        pontos += 3 + 1
-
-    if adversario[jogada] == "B":
-      if eu[jogada] == "X":
-        pontos += 3 + 2
-      elif eu[jogada] == "Y":
-        pontos += 6 + 3
-      else:
-        pontos += 0 + 1
-
-    if adversario[jogada] == "C":
-      if eu[jogada] == "X":
-        pontos += 0 + 2
-      elif eu[jogada] == "Y":
-        pontos += 3 + 3
-      else:
-        pontos += 6 + 1
-print(jogada)
+  pacote = linha
 
 
-print(pontos)
+for caractere in list(pacote):
+  arrayPacote.append(caractere)
+
+tamanhoPacote = len(arrayPacote)
+
+for i in range(tamanhoPacote):
+  if i < (tamanhoPacote - 3):
+      if arrayPacote[i] != arrayPacote[i + 1] and arrayPacote[i] != arrayPacote[i + 2] and arrayPacote[i] != arrayPacote[i + 3] and arrayPacote[i + 1] != arrayPacote[i + 2] and arrayPacote[i + 1] != arrayPacote[i + 3] and arrayPacote[i + 2] != arrayPacote[i + 3]:
+        primeiroMarcador = i + 4
+        break
+         
+
+print(primeiroMarcador)
